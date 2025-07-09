515c19a6f5d90c56cbb569e2cb049b0c2f19f761
Jonathan-CV25 <Jonathan@claervolker.com>
2025-07-09 09:25:57 +0200

    PMC: Select a Process
    Changed UI to match requirements

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.m8jgj8m1oeo"
+    "revision": "0.qd1tpepuldo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
