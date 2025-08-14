96db684cefd33849dd16e40cb5e06217fbe88d91
Jonathan-CV25 <Jonathan@claervolker.com>
2025-08-14 12:22:13 +0200

    Update wagon dispatchTimestamp to Date and sync on link
    Changed the Wagon type's dispatchTimestamp from string to Date. Updated wagon filtering logic in sampling pages to check for a valid dispatchTimestamp. In the marshaling yard wagon ID linking page, now sets dispatchTimestamp to the current date and marks wagons as pending sync when linking, ensuring accurate status tracking.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.8rtfphdemsg"
+    "revision": "0.bnmmhbdir9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
--- src/routes/pmc/processes/magnetite-rail/east-load-out/sampling/wagons/+page.svelte
+++ src/routes/pmc/processes/magnetite-rail/east-load-out/sampling/wagons/+page.svelte
@@ -119,7 +119,7 @@
 		const linkedWagons = shuntingTrain?.linkedWagons || [];
 		
 		let allwagons = (await indexedDBService.getAllRecords('wagons')).filter(
-			wagon => wagon.dispatchTimestamp !== '' && wagon.sampleTimestamp === ''
+			wagon => wagon.dispatchTimestamp && wagon.sampleTimestamp === ''
 		);
 		
 		availableWagons = allwagons.filter(
