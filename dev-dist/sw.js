c486cd6792acea421668afbb7f111181b30f8d0e
Jonathan-CV25 <Jonathan@claervolker.com>
2025-09-12 13:30:34 +0200

    Move QRPrinting to verification pages only
    Removed QrPrinting component from sampling pages and added it to verification pages for gravelotte, truck-load-out, and west-load-out processes. This change ensures QR code printing is only available during sample verification, streamlining the sampling workflow.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.a97ip8jjm8"
+    "revision": "0.8s5btek169o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
