a04b7bdeddca6bed89f595b80f66c4ebe1cb54dc
Jonathan-CV25 <Jonathan@claervolker.com>
2025-09-20 12:53:15 +0200

    Add wagonPosition to Wagon and update sorting
    Introduces the wagonPosition property to the Wagon type and ensures it is included in syncService data handling. Updates wagon listing pages to sort wagons by wagonPosition and display their position instead of index.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.3nisja6i7jo"
+    "revision": "0.v60q6lgn4dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
