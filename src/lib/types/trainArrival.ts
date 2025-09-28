2d05f316ad8f02929434e6e7110d47bf4b968da3
Jonathan-CV25 <Jonathan@claervolker.com>
2025-09-28 19:48:25 +0200

    Add train arrival verification step and update timestamps
    Introduces a verification page for train arrivals, redirecting users to it after registration. Updates TrainArrival timestamp fields to use Date objects and refactors related logic in train arrival registration to support the new flow.

--- src/lib/types/trainArrival.ts
+++ src/lib/types/trainArrival.ts
@@ -3,9 +3,9 @@ import type { BaseRecord, ID } from './baserecord';
 export interface TrainArrival extends BaseRecord {
     id: string;
 	trainId?: string;
-	portRailArrivalTimestamp?: string; // PORT_RAIL_ARRIVAL_TIMESTAMP
-	portStagingTimestamp?: string; // PORT_STAGING_TIMESTAMP
-	finishSamplingTimestamp?: string; // FINISH_SAMPLING_TIMESTAMP
+	portRailArrivalTimestamp?: Date; // PORT_RAIL_ARRIVAL_TIMESTAMP
+	portStagingTimestamp?: Date; // PORT_STAGING_TIMESTAMP
+	finishSamplingTimestamp?: Date; // FINISH_SAMPLING_TIMESTAMP
 	trainPhotoUrl?: string;
 	status: 'pending' | 'received' | 'staging' | 'sampling' | 'released';
 	serverId?: string;
