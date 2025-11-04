be3efad20b9a9ee072ad2ba962b62a8cf5fd6486
Jonathan-CV25 <Jonathan@claervolker.com>
2025-11-04 12:06:07 +0200

    Include date in loadingHour for sampling records
    Updated the sampling forms for Gravelotte, Truck Load Out, and West Load Out to prepend the current date (yyyy/mm/dd) to the loadingHour field when creating fleet records. This change ensures that the loading time is stored with an explicit date, improving data accuracy and traceability. Also fixed a minor prop binding issue in the Gravelotte sampling page.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.74rjoaf09j"
+    "revision": "0.e8rse1g47t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
