ae0dbdcbb5b6fa7e2aba548cd9cfe80f7423b933
Jonathan-CV25 <Jonathan@claervolker.com>
2025-11-06 12:50:52 +0200

    Set timeZone to UTC in date formatting
    Added the 'timeZone: UTC' option to date formatting in wagon-id-linking pages for both Bosveld and PMC processes to ensure consistent time display. Updated service worker revision for cache busting.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.k7f16c8f7sg"
+    "revision": "0.ai9hfv6jhs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
