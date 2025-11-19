4e488d67468b624c4251f15e1031c4ee18a252d9
Jonathan-CV25 <Jonathan@claervolker.com>
2025-11-19 16:27:22 +0200

    Add truck destination to fleet sampling and update logic
    Introduces a truckDestination field to the Fleet type and sampling forms for Gravelotte, including UI, localStorage, and sample number logic updates. Removes deviceCounters from IndexedDB, updates sync logic to handle truckDestination, and refines record cleanup and sample number calculation to use truckDestination. Also normalizes date parsing in related sampling pages.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.ijim8hnvncg"
+    "revision": "0.et998us830o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
