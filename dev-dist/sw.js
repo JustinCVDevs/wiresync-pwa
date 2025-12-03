58277308201a7e8426ffa860b8d38f918eee9dc4
Jonathan-CV25 <Jonathan@claervolker.com>
2025-12-03 09:31:53 +0200

    Improve QR label layout and sizing for printing
    Updated QRPrinting.svelte to generate a 3x2 inch label at 300 DPI with a larger QR code and centered Sample ID text. Adjusted canvas and image styles to match new label dimensions for better print quality and consistency.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.g3oh3d6jh1"
+    "revision": "0.t0qtcncjcn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
