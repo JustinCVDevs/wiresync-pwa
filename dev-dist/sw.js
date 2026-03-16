1d9637c427bd4a27940ef1371393924d2a6f0eeb
Jonathan-CV25 <Jonathan@claervolker.com>
2026-03-16 14:00:21 +0200

    Upgrade rollup/adapter-node and refactor wagon UI
    Bump @sveltejs/adapter-node to ^5.5.4 in package.json and update lockfile (rollup and related packages updated accordingly). Update service worker precache revision in dev-dist/sw.js. Refactor the train-sampling wagons page: default trainRefNr to an empty string, rename step label to "Wagon", add showSearch state, simplify onMount logic to resolve linked train/trainArrival and compute available wagons, replace the manual wagon autocomplete and sampleId input with a FormField in search mode populated from availableWagons, and remove the old suggestion list/selection handlers and sampleId write on submit. These changes simplify the UI and align dependency updates.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.kolpucvukq8"
+    "revision": "0.rs9uj83ne1k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
