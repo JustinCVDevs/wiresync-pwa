ad4977e9b6dae35d9aae0dd6464143f2a57b248c
Jonathan Faurie <Jonathan@claervolker.com>
2026-06-04 09:19:40 +0200

    Uncomment icon buttons in wagon edit pages
    Restore previously commented-out icon markup in the wagon edit pages for both bosveld and pmc marshaling-yard wagon ID linking. The <svelte:fragment slot="field1-icon"> block and the inline icon button next to the RFID input were uncommented so the field icons render again; no functional logic was changed.

--- src/routes/bosveld/processes/marshaling-yard/wagon-id-linking/wagons/[trainId]/edit/[wagonId]/+page.svelte
+++ src/routes/bosveld/processes/marshaling-yard/wagon-id-linking/wagons/[trainId]/edit/[wagonId]/+page.svelte
@@ -359,7 +359,7 @@
 							error={showRfidError ? rfidErrorMsg : ''}
 							on:field2focus={() => { showRfidError = false; }}
 						>
-							<!-- <svelte:fragment slot="field1-icon">
+							<svelte:fragment slot="field1-icon">
 								<button
 									type="button"
 									class="absolute right-2 top-1/2 -translate-y-1/2 p-1 text-gray-400 hover:text-gray-600"
@@ -370,7 +370,7 @@
 										<path stroke-linecap="round" stroke-linejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 16.5h.75v.75h-.75v-.75Z" />
 									</svg>
 								</button>
-							</svelte:fragment> -->
+							</svelte:fragment>
 						</DoubleConfirmField>
 					{:else}
 						<div class="space-y-1">
@@ -385,7 +385,7 @@
 									on:focus={() => { rfidFocused = true; }}
 								/>
 								<!-- svelte-ignore a11y_consider_explicit_label -->
-								<!-- <button
+								<button
 									type="button"
 									class="absolute right-2 top-1/2 -translate-y-1/2 p-1 text-gray-400 hover:text-gray-600"
 									tabindex="-1"
@@ -395,7 +395,7 @@
 										<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z" />
 										<path stroke-linecap="round" stroke-linejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 16.5h.75v.75h-.75v-.75Z" />
 									</svg>
-								</button> -->
+								</button>
 							</div>
 						</div>
 					{/if}
