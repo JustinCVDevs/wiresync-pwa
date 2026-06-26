f9ab7616d9b77b5bc56012f341b1e8543b6fff0e
Jonathan Faurie <Jonathan@claervolker.com>
2026-06-26 10:10:53 +0200

    Default to All wagon list
    Remove auto going to missing wagons for both PMC and Bosveld

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.euh9iuc9dco"
+    "revision": "0.kcn9mjo494s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
